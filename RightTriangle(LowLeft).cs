5c7c9e8f2543296cdb53dc6b9e58ae75839855c5
Ruel Pamaran <147482226+weng2806@users.noreply.github.com>
2024-10-14 19:56:33 +0800

    Create RightTriangle(LowLeft).cs

---
+++ RightTriangle(LowLeft).cs
@@ -0,0 +1,19 @@
+i = 5;
+while (i >= 1)
+{
+    int j = 1;
+    while (j <= i)
+    {
+        Console.Write("*");
+        j++;
+    }
+    Console.WriteLine();
+    i--;
+}
+/* 
+*****
+****
+***
+**
+*
+*/
